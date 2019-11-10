2b0182be26f753045ec397b9138a90903199611f
Ngoc Ky <ngockya98@gmail.com>
2019-11-10 17:05:02 +0700

    update 1.0.4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,10 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-<<<<<<< HEAD
 
-=======
->>>>>>> e449ebf53097cf9ae969ccee9a4aebd9bd6cb5b4
 class AppServiceProvider extends ServiceProvider
 {
     /**
